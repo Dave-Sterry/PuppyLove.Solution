1e3f077de6945ce691a7b61e8178bf2eb6663b7c
David Sterry <sterry.david@gmail.com>
2021-01-25 14:00:27 -0800

    add more dogs to database
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

---
+++ PuppyLove/Migrations/20210125215750_MoreDogs.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PuppyLove.Migrations
+{
+    public partial class MoreDogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "ImgUrl", "Location", "Mood", "Name", "OwnerName", "Size" },
+                values: new object[,]
+                {
+                    { 3, 1, "Husky", "https://share.icloud.com/photos/0mxrEdflh8fSsVDXz6QaL5X-A#Oak_Hills", "Portland, OR", "Inquisitive, playful", "Fin", "Jerrod", "Small" },
+                    { 4, 2, "Mized", "", "Seattle, WA", "Hyper and wild", "Jeans", "Heather", "Medium" },
+                    { 5, 2, "Great Pyrenees ", "https://photos.app.goo.gl/H8VkzPHeg1M6oWa38", "Portland, OR", "Sweethearted goofus", "Remy", "Marci", "Big" },
+                    { 6, 12, "Irish Setter", "https://photos.app.goo.gl/AqDXPTwJBJTzQP2s7", "Portland, OR", "Loveable dunderhead", "Neville", "Travis", "Large" },
+                    { 7, 13, "Belgian Shepard", "https://drive.google.com/file/d/1k-XzE-I0BPJK7RCZw0pfzIx0EO30IAEp/view?usp=sharing", "Portland, OR", "Adorable old man", "Jasper", "Erin", "Large" },
+                    { 8, 11, "Chihuaua", "https://photos.app.goo.gl/iBdKw2wsPtGdJjSL7", "Portland, OR", "Sassy princess", "Truvie", "Luke", "Tiny" },
+                    { 9, 1, "Corgi", "https://drive.google.com/file/d/1k-XzE-I0BPJK7RCZw0pfzIx0EO30IAEp/view?usp=sharing", "Portland, OR", "High energy joy", "Opal", "Sarah", "small" },
+                    { 10, 3, "German Sheppard", "https://photos.app.goo.gl/FbVEStTT3a198rua6", "Seaettle, WA", "Anxious sweet boi", "Link", "Kris", "Big" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 10);
+        }
+    }
+}
--- PuppyLove/Models/PuppyLoveContext.cs
+++ PuppyLove/Models/PuppyLoveContext.cs
@@ -16,7 +16,15 @@ namespace PuppyLove.Models
                 .HasData(
                     //new Dog { DogId = , Name = "", OwnerName = "", Mood = "", Age = , Breed = "", Size = "", Location = "", ImgUrl = ""}
                     new Dog { DogId = 1, Name = "Helo", OwnerName = "Dave", Mood = "Upbeat and calm", Age = 8, Breed = "Mixed", Size = "Medium", Location = "Portland, OR", ImgUrl = "https://drive.google.com/file/d/12-NJjKT1mrCuA1lDzVCF9-1nec04oydw/view?usp=sharing"},
-                    new Dog { DogId = 2 , Name = "Bartleby", OwnerName = "Sage", Mood = "Snarky", Age = 2 , Breed = "Mixed", Size = "Small", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/3sn4vdSiKDbLutba8"}
+                    new Dog { DogId = 2 , Name = "Bartleby", OwnerName = "Sage", Mood = "Snarky", Age = 2 , Breed = "Mixed", Size = "Small", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/3sn4vdSiKDbLutba8"},
+                    new Dog { DogId =3 , Name = "Fin", OwnerName = "Jerrod", Mood = "Inquisitive, playful", Age = 1 , Breed = "Husky", Size = "Small", Location = "Portland, OR", ImgUrl = "https://share.icloud.com/photos/0mxrEdflh8fSsVDXz6QaL5X-A#Oak_Hills"},
+                    new Dog { DogId = 4, Name = "Jeans", OwnerName = "Heather", Mood = "Hyper and wild", Age = 2, Breed = "Mized", Size = "Medium", Location = "Seattle, WA", ImgUrl = ""},
+                    new Dog { DogId = 5, Name = "Remy", OwnerName = "Marci", Mood = "Sweethearted goofus", Age = 2 , Breed = "Great Pyrenees ", Size = "Big", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/H8VkzPHeg1M6oWa38"},
+                    new Dog { DogId = 6, Name = "Neville", OwnerName = "Travis", Mood = "Loveable dunderhead", Age = 12, Breed = "Irish Setter", Size = "Large", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/AqDXPTwJBJTzQP2s7"},
+                    new Dog { DogId = 7 , Name = "Jasper", OwnerName = "Erin", Mood = "Adorable old man", Age = 13, Breed = "Belgian Shepard", Size = "Large", Location = "Portland, OR", ImgUrl = "https://drive.google.com/file/d/1k-XzE-I0BPJK7RCZw0pfzIx0EO30IAEp/view?usp=sharing"},
+                    new Dog { DogId = 8, Name = "Truvie", OwnerName = "Luke", Mood = "Sassy princess", Age = 11 , Breed = "Chihuaua", Size = "Tiny", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/iBdKw2wsPtGdJjSL7"},
+                    new Dog { DogId = 9 , Name = "Opal", OwnerName = "Sarah", Mood = "High energy joy", Age = 1, Breed = "Corgi", Size = "small", Location = "Portland, OR", ImgUrl = "https://drive.google.com/file/d/1k-XzE-I0BPJK7RCZw0pfzIx0EO30IAEp/view?usp=sharing"},
+                    new Dog { DogId = 10, Name = "Link", OwnerName = "Kris", Mood = "Anxious sweet boi", Age = 3, Breed = "German Sheppard", Size = "Big", Location = "Seaettle, WA", ImgUrl = "https://photos.app.goo.gl/FbVEStTT3a198rua6"}
                 );
         }
     }
