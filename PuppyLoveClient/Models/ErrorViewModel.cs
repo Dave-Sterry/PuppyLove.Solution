438198cf8a349efeccfc4508843ce24a89d94988
David Sterry <sterry.david@gmail.com>
2021-01-25 09:29:21 -0800

    initialize project
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

---
+++ PuppyLoveClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PuppyLoveClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
