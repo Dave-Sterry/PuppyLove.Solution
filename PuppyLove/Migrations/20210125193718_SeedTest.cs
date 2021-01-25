4e808cdc2cc0718fb8a7742a76af01bf90829c01
David Sterry <sterry.david@gmail.com>
2021-01-25 11:43:35 -0800

    seed test data with image links, check in postman
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

---
+++ PuppyLove/Migrations/20210125193718_SeedTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PuppyLove.Migrations
+{
+    public partial class SeedTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "ImgUrl", "Location", "Mood", "Name", "OwnerName", "Size" },
+                values: new object[] { 1, 8, "Mixed", "https://drive.google.com/file/d/12-NJjKT1mrCuA1lDzVCF9-1nec04oydw/view?usp=sharing", "Portland, OR", "Upbeat and calm", "Helo", "Dave", "Medium" });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "ImgUrl", "Location", "Mood", "Name", "OwnerName", "Size" },
+                values: new object[] { 2, 2, "Mixed", "https://photos.app.goo.gl/3sn4vdSiKDbLutba8", "Portland, OR", "Snarky", "Bartleby", "Sage", "Small" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+        }
+    }
+}
--- PuppyLove/Models/PuppyLoveContext.cs
+++ PuppyLove/Models/PuppyLoveContext.cs
@@ -9,5 +9,15 @@ namespace PuppyLove.Models
         }
 
         public DbSet<Dog> Dogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Dog>()
+                .HasData(
+                    //new Dog { DogId = , Name = "", OwnerName = "", Mood = "", Age = , Breed = "", Size = "", Location = "", ImgUrl = ""}
+                    new Dog { DogId = 1, Name = "Helo", OwnerName = "Dave", Mood = "Upbeat and calm", Age = 8, Breed = "Mixed", Size = "Medium", Location = "Portland, OR", ImgUrl = "https://drive.google.com/file/d/12-NJjKT1mrCuA1lDzVCF9-1nec04oydw/view?usp=sharing"},
+                    new Dog { DogId = 2 , Name = "Bartleby", OwnerName = "Sage", Mood = "Snarky", Age = 2 , Breed = "Mixed", Size = "Small", Location = "Portland, OR", ImgUrl = "https://photos.app.goo.gl/3sn4vdSiKDbLutba8"}
+                );
+        }
     }
 }
